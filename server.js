9f4e78f34ee5f208c0d73c45cb17aa740b6914e2
tls <thelearningstationhsr@gmail.com>
2025-04-14 15:27:29 +0530

    frontend link updated

--- server.js
+++ server.js
@@ -9,18 +9,20 @@ connectDB();
 
 const app = express();
 app.use(cors());
-// app.use(cors({ origin: "http://localhost:3000" }));
+//app.use(cors({ origin: "http://localhost:3000" }));
 app.use(cors({ origin: "https://janavaasyojnaharyana.com" }));
 
-app.use(cors({
-    origin: 'http://localhost:3001', // or your frontend URL when deployed
-    credentials: true
-  }));
+// app.use(cors({
+//     origin: 'http://localhost:3001', // or your frontend URL when deployed
+//     credentials: true
+//   }));
+
+
 
 app.use(express.json());
 
 app.use("/api/shop", require("./routes/shopRoutes"));
 
-const PORT = 3000 //process.env.PORT;
+const PORT = 5000 //process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 module.exports = app; //  Vercel ke liye export kar diya
